5f7abc72d1741f76514b018ab3a322d9f34f4c84
clainerhenrique <clainerhenrique.o@gmail.com>
2023-04-12 19:59:53 -0300

    Segundo commit add exercicios

--- 1129.c
+++ 1129.c
@@ -5,34 +5,34 @@ int main()
    int j,i,n,a,b,c,d,e;
    /*menor ou igual a 127 =  preto;
      maior que 127 = branco */
-    for(j = 0 ; j < 100 ; j++){
-     scanf("%d", &n);
-        if (n == 0){
-        break;
-     }
-        else {
-    for(i = 0 ; i < n ; i++){
-     scanf("%d %d %d %d %d", &a,&b,&c,&d,&e);
-    if(a <= 127 && b > 127 && c > 127 && d > 127 && e > 127 ){
-    printf("A\n");
+      for(j = 0 ; j < 100 ; j++){
+      scanf("%d", &n);
+      if (n == 0){
+      break;
+      }
+      else {
+      for(i = 0 ; i < n ; i++){
+      scanf("%d %d %d %d %d", &a,&b,&c,&d,&e);
+      if(a <= 127 && b > 127 && c > 127 && d > 127 && e > 127 ){
+      printf("A\n");
+      }
+      else if(a > 127 && b <= 127 && c >127 && d > 127 && e > 127){
+      printf("B\n");
+      }
+      else if(a > 127 && b > 127 && c <= 127 && d > 127 && e > 127){
+      printf("C\n");
+      }
+      else if(a > 127 && b > 127 && c > 127 && d <= 127 && e > 127){
+      printf("D\n");
+      }
+      else if(a > 127 && b > 127 && c > 127 && d > 127 && e <= 127){
+      printf("E\n");
+      }
+      else{
+      printf("*\n");
+      }
    }
-   else if(a > 127 && b <= 127 && c >127 && d > 127 && e > 127){
-    printf("B\n");
-   }
-   else if(a > 127 && b > 127 && c <= 127 && d > 127 && e > 127){
-    printf("C\n");
-   }
-    else if(a > 127 && b > 127 && c > 127 && d <= 127 && e > 127){
-    printf("D\n");
-   }
-   else if(a > 127 && b > 127 && c > 127 && d > 127 && e <= 127){
-    printf("E\n");
-   }
-   else{
-    printf("*\n");
-   }
-   }
-     }
-    }
-    return 0;
+      }
+      }
+      return 0;
 }
